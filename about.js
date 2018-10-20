3de2f246e0dc0457acfd9817bc88f1f165523c59
annanican <anna.nicanorova@gmail.com>
2018-10-20 10:40:44 -0400

    resturctured the app in node.js

---
+++ about.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+  </head>
+  <body>
+  	<H2>Hello world</H2>
+  </body>
+</html>
--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
